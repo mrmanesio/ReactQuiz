39b20a70eaa6651d61f5e50e189c997c47f93939
manesio2009@gmail.com <manesio2009@gmail.com>
2021-01-16 20:38:26 +0200

    add redux/thunk

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReduser from './store/reducers/rootReduser';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const store = createStore(
+  rootReduser,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
 
 const app = (
-  <BrowserRouter>
+  <Provider store={store}>
+<BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
+  
 )
 
 ReactDOM.render(app, document.getElementById('root'));
